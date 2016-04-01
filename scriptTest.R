4dd729e88b7977d52722b078215757a171524f49
Wesley <wesley.nmtorres@gmail.com>
2016-04-01 18:06:42 -0300

    script para regressão

---
+++ scriptTest.R
@@ -0,0 +1,16 @@
+dados <- read.table("~/workspaceR/linearRegression/data/prostate.data")
+df <- melt(dados)
+plot(dados$lcavol, dados$lpsa, main="Scatterplot Example", xlab="lcavol ", ylab="lpsa", pch=19)
+
+ggplot(df,aes(x = value)) + facet_wrap(~variable, scales = "free_x") + geom_histogram()
+correlationMatrix <- cor(dados)
+corrplot(correlationMatrix, method="circle", type="lower", order="hclust")
+
+train <- filter(dados,train)
+test <- filter(dados,!train)
+lm <- lm(lpsa ~ lcavol+svi+lcp,data = train)
+prediction <- predict(lm, select(test,lcavol, svi, lcp))
+lm_prediction <- data.frame(pred = prediction, obs = test$lpsa)
+ggplot(lm_prediction, aes(x = pred, y = obs)) + geom_point(alpha = 0.5, position = position_jitter(width=0.2)) + geom_abline(colour = "blue") + ggtitle("Previsão x Observado (validação)")
+round(defaultSummary(lm_prediction), digits = 3)
+corrplot(correlationMatrix, method="circle", type="lower", order="hclust")
